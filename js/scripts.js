db2fd9f62d4cc45f939838a96e00f429207cbcb7
GodLSan <lessandrof@gmail.com>
2025-12-05 08:30:08 -0300

    exercício 2 comentado Inner

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,20 @@
 function trocarCores() {
 
-    let itens = document.querySelectorAll('li');
+    let itens = document.getElementsByTagName('li');
 
-    itens.forEach(item => {
+    for (let i = 0; i < itens.length; i++) {
+        let item = itens[i]; 
 
         if (item.classList.contains('azul')) {
             item.classList.remove('azul');
             item.classList.add('vermelho');
+            
+            /* item.innerHTML = "Item Vermelho "; */
         } else {
             item.classList.remove('vermelho');
             item.classList.add('azul');
+            
+            /* item.textContent = "Item Azul "; */
         }
-    });
+    }
 }
